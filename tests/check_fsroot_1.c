52b531b03ab654a7be7916a0dbaaa5f341fcb88f
Ander Juaristi <ajuaristi@gmx.es>
2017-01-25 16:44:04 +0100

    Fix test stories
     * tests/check_fsroot_1.c (test_fsroot_create): take directory path
       from global variable 'dir'.
       (test_fsroot_open): call create/write/release before fsroot_open().
       Fill buf with zeros before fsroot_read().

--- tests/check_fsroot_1.c
+++ tests/check_fsroot_1.c
@@ -1,8 +1,12 @@
 #include <check.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "../fsroot.h"
 
+char dir[] = "fsroot-root";
 char foo_data[] = "Hello, world! This is 'foo'.",
 	bar_data[] = "Hi, I'm 'bar'!";
 
@@ -11,7 +15,7 @@ START_TEST(test_fsroot_create)
 	int retval, err = 0;
 	int fd_foo, fd_bar;
 
-	retval = fsroot_init("fsroot-root");
+	retval = fsroot_init(dir);
 	ck_assert_msg(retval == FSROOT_OK, "fsroot_init() returned %d\n", retval);
 
 	fd_foo = fsroot_create("foo", 1000, 1000, 0100700, O_CREAT | O_RDWR, &err);
@@ -43,8 +47,26 @@ START_TEST(test_fsroot_open)
 	char buf[500];
 	size_t bufsize = sizeof(buf);
 
-	retval = fsroot_init("fsroot-root");
+	retval = fsroot_init(dir);
 	ck_assert_msg(retval == FSROOT_OK, "fsroot_init() returned %d\n", retval);
+
+	fd_foo = fsroot_create("foo", 1000, 1000, 0100700, O_CREAT | O_RDWR, &err);
+	ck_assert_msg(fd_foo >= 0, "fsroot_create(\"foo\") returned %d (err: %d)\n", fd_foo, err);
+	fd_bar = fsroot_create("bar", 1000, 1000, 0100700, O_RDWR, &err);
+	ck_assert_msg(fd_bar >= 0 && fd_bar != fd_foo, "fsroot_create(\"bar\") returned %d (err: %d)\n",
+			fd_bar, err);
+
+	retval = fsroot_write(fd_foo, foo_data, sizeof(foo_data) - 1, 0, &err);
+	ck_assert_msg(retval == (sizeof(foo_data) - 1), "fsroot_write(\"foo\") returned %d (err: %d)\n",
+			retval, err);
+	retval = fsroot_write(fd_bar, bar_data, sizeof(bar_data) - 1, 0, &err);
+	ck_assert_msg(retval == (sizeof(bar_data) - 1), "fsroot_write(\"bar\") returned %d (err: %d)\n",
+			retval, err);
+
+	retval = fsroot_release("foo");
+	ck_assert_msg(retval == FSROOT_OK, "fsroot_release(\"foo\") returned %d\n", retval);
+	retval = fsroot_release("bar");
+	ck_assert_msg(retval == FSROOT_OK, "fsroot_release(\"bar\") returned %d\n", retval);
 
 	fd_foo = fsroot_open("foo", O_CREAT | O_RDONLY);
 	ck_assert_msg(fd_foo >= 0, "fsroot_open(\"foo\") returned %d\n",
@@ -53,12 +75,14 @@ START_TEST(test_fsroot_open)
 	ck_assert_msg(fd_bar >= 0 && fd_bar != fd_foo, "fsroot_open(\"bar\") returned %d\n",
 			fd_bar);
 
+	memset(buf, 0, bufsize);
 	retval = fsroot_read(fd_foo, buf, bufsize, 0, &err);
-	ck_assert_msg(retval != strlen(foo_data), "fsroot_read(\"foo\") returned %d (err: %d)\n",
+	ck_assert_msg(retval == strlen(foo_data), "fsroot_read(\"foo\") returned %d (err: %d)\n",
 			retval, err);
 	ck_assert_str_eq(buf, foo_data);
+	memset(buf, 0, bufsize);
 	retval = fsroot_read(fd_bar, buf, bufsize, 0, &err);
-	ck_assert_msg(retval != strlen(bar_data), "fsroot_read(\"bar\") returned %d (err: %d)\n",
+	ck_assert_msg(retval == strlen(bar_data), "fsroot_read(\"bar\") returned %d (err: %d)\n",
 			retval, err);
 	ck_assert_str_eq(buf, bar_data);
 
@@ -71,6 +95,29 @@ START_TEST(test_fsroot_open)
 }
 END_TEST
 
+void empty_dir()
+{
+	DIR *d;
+	struct dirent *de = NULL;
+	char path[PATH_MAX];
+
+	d = opendir(dir);
+	if (!d)
+		ck_abort_msg("Could not open directory '%s'\n", dir);
+
+	do {
+		de = readdir(d);
+		if (de && strcmp(de->d_name, ".") && strcmp(de->d_name, "..")) {
+			ck_assert(snprintf(path, sizeof(path), "%s/%s", dir, de->d_name) > 0);
+			ck_assert_msg(unlink(path) == 0,
+					"Could not unlink file '%s'\n",
+					de->d_name);
+		}
+	} while (de);
+
+	closedir(d);
+}
+
 Suite *fsroot_suite()
 {
 	Suite *s;
@@ -80,6 +127,8 @@ Suite *fsroot_suite()
 
 	tc = tcase_create("core");
 	tcase_add_test(tc, test_fsroot_create);
+	tcase_add_test(tc, test_fsroot_open);
+	tcase_add_checked_fixture(tc, NULL, empty_dir);
 
 	suite_add_tcase(s, tc);
 	return s;
